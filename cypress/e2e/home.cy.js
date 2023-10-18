6391aa60aab0543c918ff3bd05a5fd748185cda2
t-montes <t.montes@uniandes.edu.co>
2023-10-18 00:33:38 -0500

    detail-tests

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,13 +1,14 @@
-
 describe('Home Page', () => {
-  it("should show carousel items by default", () => {
+  it('should show carousel items by default', () => {
     cy.visit('https://www.mercadolibre.com.co/');
 
     cy.get('div.ui-recommendations-carousel-snapped').should('exist');
-    cy.get('div.ui-recommendations-carousel-snapped').find('div.andes-carousel-snapped__slide').should('exist');
+    cy.get('div.ui-recommendations-carousel-snapped')
+      .find('div.andes-carousel-snapped__slide')
+      .should('exist');
   });
 
-  it("should show categories", () => {
+  it('should show categories', () => {
     cy.visit('https://www.mercadolibre.com.co/');
 
     cy.get('a.nav-menu-categories-link').click();
@@ -32,7 +33,7 @@ describe('Home Page', () => {
 });
 
 describe('Search', () => {
-  it("should show search results", () => {
+  it('should show search results', () => {
     cy.visit('https://www.mercadolibre.com.co/');
 
     cy.get('input.nav-search-input').type('iphone');
@@ -48,7 +49,9 @@ describe('Search', () => {
     cy.get('button.nav-search-btn').click();
 
     cy.get('div.ui-search-result__wrapper').should('exist');
-    cy.get('h2.ui-search-item__title').first().contains(/iphone|Iphone|iPhone/);
+    cy.get('h2.ui-search-item__title')
+      .first()
+      .contains(/iphone|Iphone|iPhone/);
   });
 
   it("should show search results for 'FGHJK'", () => {
@@ -63,7 +66,7 @@ describe('Search', () => {
 });
 
 describe('Product Detail', () => {
-  it("should show product detail", () => {
+  it('should show product detail', () => {
     cy.visit('https://www.mercadolibre.com.co/');
 
     cy.get('input.nav-search-input').type('iphone');
@@ -75,7 +78,7 @@ describe('Product Detail', () => {
     cy.get('div.ui-pdp-container').should('exist');
   });
 
-  it("product detail title should be the same as the one clicked", () => {
+  it('product detail title should be the same as the one clicked', () => {
     cy.visit('https://www.mercadolibre.com.co/');
 
     cy.get('input.nav-search-input').type('iphone');
@@ -83,41 +86,49 @@ describe('Product Detail', () => {
 
     cy.get('div.ui-search-result__wrapper').should('exist');
     // save the title of the first product, in a variable
-    cy.get('h2.ui-search-item__title').first().then(($title) => {
-      const title = $title.text();
-      cy.get('h2.ui-search-item__title').first().click();
-      // verify that the title of the product detail is the same as the one saved
-      cy.get('div.ui-pdp-container').should('exist');
-      cy.get('h1.ui-pdp-title').should('contain', title);
-    });
-
+    cy.get('h2.ui-search-item__title')
+      .first()
+      .then(($title) => {
+        const title = $title.text();
+        cy.get('h2.ui-search-item__title').first().click();
+        // verify that the title of the product detail is the same as the one saved
+        cy.get('div.ui-pdp-container').should('exist');
+        cy.get('h1.ui-pdp-title').should('contain', title);
+      });
   });
 
-  
-});
+  it("product detail should show 'Comprar ahora' button", () => {
+    cy.visit('https://www.mercadolibre.com.co/');
 
+    cy.get('input.nav-search-input').type('iphone');
+    cy.get('button.nav-search-btn').click();
 
-/*
-// Test to check if youtube.com renders videos by default
-describe('Youtube homepage', () => {
-  it('should render videos by default', () => {
-    cy.visit('https://www.youtube.com/');
-    cy.get('#contents').should('exist');
+    cy.get('div.ui-search-result__wrapper').should('exist');
+    cy.get('h2.ui-search-item__title')
+      .first()
+      .then(($title) => {
+        cy.get('h2.ui-search-item__title').first().click();
+        cy.get('button.ui-pdp-action--primary').should('exist');
+      });
   });
-});
 
-// Test to check if login functionality works
-describe('Youtube login', () => {
-  it('should login successfully', () => {
-    cy.visit('https://www.youtube.com/');
-    cy.get('#buttons > ytd-button-renderer:nth-child(2) > a').click();
-    cy.get('#identifierId').type('your_username');
-    cy.get('#identifierNext > div > button').click();
-    cy.get('#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input').type(
-      'your_password'
-    );
-    cy.get('#passwordNext > div > button').click();
-    cy.url().should('include', 'https://www.youtube.com/');
+  it("after clicking 'Comprar ahora' button, should show login page", () => {
+    cy.visit('https://www.mercadolibre.com.co/');
+
+    cy.get('input.nav-search-input').type('iphone');
+    cy.get('button.nav-search-btn').click();
+
+    cy.get('div.ui-search-result__wrapper').should('exist');
+    cy.get('h2.ui-search-item__title')
+      .first()
+      .then(($title) => {
+        cy.get('h2.ui-search-item__title').first().click();
+        cy.get('button.ui-pdp-action--primary').click();
+
+        cy.origin('https://www.mercadolibre.com/', () => {
+          cy.get('h1.center-card__title').should('contain', '¡Hola! Para comprar, ingresa a tu cuenta');
+        });
+      });
   });
+
 });
-*/
